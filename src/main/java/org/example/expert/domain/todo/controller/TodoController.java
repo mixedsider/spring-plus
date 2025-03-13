ccef486fe9f2d0dfc91135b074fee0fa5a950bb5
mixedsider <dmstjd0812@gmail.com>
2025-03-13 19:03:40 +0900

    feat(lv.3, lv.4) : 기능 수정
    ### todo 페이징 기능 추가
    - weather, Date 를 기준으로 조회 가능
    
    ### Todo Controller 수정
    - OK -> BAD_REQUEST로 변경

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -1,20 +1,30 @@
 package org.example.expert.domain.todo.controller;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.todo.dto.request.TodoRequest;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
 import org.example.expert.domain.todo.service.TodoService;
 import org.springframework.data.domain.Page;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
+
+    private static final String DEFAULT_START_DATE = "1999-01-01T00:00:00";
+    private static final String DEFAULT_END_DATE = "2999-12-31T23:59:59";
 
     private final TodoService todoService;
 
@@ -29,9 +39,12 @@ public class TodoController {
     @GetMapping("/todos")
     public ResponseEntity<Page<TodoResponse>> getTodos(
             @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
-    ) {
-        return ResponseEntity.ok(todoService.getTodos(page, size));
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String weather,
+            @RequestParam(required = false, defaultValue = DEFAULT_START_DATE) LocalDateTime startDate,
+            @RequestParam(required = false, defaultValue = DEFAULT_END_DATE) LocalDateTime endDate
+            ) {
+        return ResponseEntity.ok(todoService.getTodos(page, size, weather, startDate, endDate));
     }
 
     @GetMapping("/todos/{todoId}")

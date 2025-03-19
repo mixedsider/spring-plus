cf7d47bc076181151bcc942bf36f7e715977eb7f
mixedsider <dmstjd0812@gmail.com>
2025-03-19 17:52:24 +0900

    feat(lv.8) : QueryDSL 해결

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -48,7 +48,7 @@ public class TodoController {
     }
 
     @GetMapping("/todos/{todoId}")
-    public ResponseEntity<TodoResponse> getTodo(@PathVariable long todoId) {
+    public ResponseEntity<TodoResponse> getTodo(@PathVariable(value = "todoId") long todoId) {
         return ResponseEntity.ok(todoService.getTodo(todoId));
     }
 }

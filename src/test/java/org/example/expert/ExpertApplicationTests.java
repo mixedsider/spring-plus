31d37321e231a78b4e01273e43d26ce38e32354a
mixedsider <dmstjd0812@gmail.com>
2025-03-21 15:59:12 +0900

    fix(user): 컨트롤러 테스트 api 시간 리턴 추가

--- src/test/java/org/example/expert/ExpertApplicationTests.java
+++ src/test/java/org/example/expert/ExpertApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.expert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ExpertApplicationTests.class)
 class ExpertApplicationTests {
 
     @Test
